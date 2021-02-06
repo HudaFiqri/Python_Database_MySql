8c1eba1fac469e6af91f15a375a39cd017d6691b
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-06 22:41:49 +0800

    Update beberapa line code

--- Check Connection/main.py
+++ Check Connection/main.py
@@ -1,10 +1,13 @@
+###import module yang diperlukan
 import mysql.connector
 
+###membuat config authentikasi
 username = 'cperoot'
 password = 'nasional123'
 server = 'localhost'
 database = 'Local'
 
+###testing koneksi dengan server apakah berhasil atau tida
 try:
     DB_Connect = mysql.connector.connect(user=username, password=password, host=server, database=database)
 
