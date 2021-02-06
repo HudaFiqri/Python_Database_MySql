988b6ae25e1124afae77efab5128b5af03b568fe
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-06 22:31:40 +0800

    Update main.py

--- Insert Data Picture/main.py
+++ Insert Data Picture/main.py
@@ -4,7 +4,6 @@ import Connection
 DB_Data = 'https://cupline.files.wordpress.com/2014/10/logo.gif'
 DB_Image_Name = 'Debian 9 Image'
 
-#INSERT INTO Other_Data (Name) VALUES ('http://1.bp.blogspot.com/-q2w0RBF6eSA/UylHlQHr-WI/AAAAAAAACnE/ZL7HpBFko6k/s280/smadav-terbaru.jpg')
 DB_Query = f"INSERT INTO Other_Data (Name, Picture) VALUES ('{DB_Image_Name}', '{DB_Data}')"
 
 ###eksekusi perintah ke server
